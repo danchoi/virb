2045e0e4c68011ae5980e917878c9b965382bedc
Daniel Choi <dhchoi@gmail.com>
2012-12-24 09:26:00 -0500

    move files around

--- lib/virb/pry.rb
+++ lib/virb/pry.rb
@@ -5,73 +5,77 @@ unless File.exist?('.virb/fifo')
 end
 `touch .virb/session`
 
-#`mkfifo fifo`
-class Fifo
-  def initialize
-    reinit_fifo
-  end
-  def reinit_fifo
-    fd = IO.sysopen(".virb/fifo")
-    @io = IO.new(fd, 'r')
-  end
-  def readline(current_prompt)
-    $stdout.print current_prompt
-    if @io.eof?
-      reinit_fifo
+module Virb
+  class Pry
+    class Fifo
+      def initialize
+        reinit_fifo
+      end
+      def reinit_fifo
+        fd = IO.sysopen(".virb/fifo")
+        @io = IO.new(fd, 'r')
+      end
+      def readline(current_prompt)
+        $stdout.print current_prompt
+        if @io.eof?
+          reinit_fifo
+        end
+        x = @io.gets
+        $stdout.print x
+        x
+      end
+      def rewind
+      end
     end
-    x = @io.gets
-    $stdout.print x
-    x
-  end
-  def rewind
+
+    # fakes a tty stdout
+    class Output
+      def initialize
+        @out = File.open(".virb/session", "w")
+        @out.sync = true
+      end
+      def print s
+        @out.print s
+      end
+      def puts s
+        @out.puts s
+      end
+      def write s
+        @out.write s
+      end
+      def flush
+        @out.flush
+      end
+      def tty?
+        true
+      end
+    end
+
+    def self.start
+      pgid = Process.getpgid(Process.pid())
+
+      pid = fork do
+        ENV['TERM'] = 'dumb'
+        require 'pry'
+
+        out = Virb::Pry::Output.new
+        $stdout = out
+        $stderr = out
+
+        ::Pry.cli = true
+        ::Pry.color = false
+        ::Pry.config.pager = false
+        ::Pry.config.print = ::Pry::SIMPLE_PRINT
+        ::Pry.config.auto_indent = false # turns off ansi control escape sequences
+        input = Fifo.new
+        ::Pry.input = input
+        ::Pry.config.output = out
+        ::Pry.start(TOPLEVEL_BINDING.eval('self'))
+      end
+
+      args = ARGV.dup
+      vimscript = File.join(File.dirname(__FILE__), '..', 'virb.vim')
+      exec("vim -S #{vimscript} #{args.join(' ')} && rm -rf .virb && kill 0")
+    end
   end
 end
-
-# fakes a tty stdout
-class Output
-  def initialize
-    @out = File.open(".virb/session", "w")
-    @out.sync = true
-  end
-  def print s
-    @out.print s
-  end
-  def puts s
-    @out.puts s
-  end
-  def write s
-    @out.write s
-  end
-  def flush
-    @out.flush
-  end
-  def tty?
-    true
-  end
-end
-
-pgid = Process.getpgid(Process.pid())
-
-pid = fork do
-  ENV['TERM'] = 'dumb'
-  require 'pry'
-
-  out = Output.new
-  $stdout = out
-  $stderr = out
-
-  Pry.cli = true
-  Pry.color = false
-  Pry.config.pager = false
-  Pry.config.print = Pry::SIMPLE_PRINT
-  Pry.config.auto_indent = false # turns off ansi control escape sequences
-  input = Fifo.new
-  Pry.input = input
-  Pry.config.output = out
-  Pry.start(TOPLEVEL_BINDING.eval('self'))
-end
-
-args = ARGV.dup
-vimscript = File.join(File.dirname(__FILE__), '..', 'virb.vim')
-exec("vim -S #{vimscript} #{args.join(' ')} && rm -rf .virb && kill 0")
-
--- lib/virb/railtie.rb
+++ lib/virb/railtie.rb
@@ -2,18 +2,36 @@
 
 module Virb
   class Railtie < Rails::Railtie
-    console do
-      if Rails::VERSION::MAJOR == 3
-        Rails::Console::IRB = IRB
-      end  
-      if Rails::VERSION::MAJOR == 4
-        Rails.application.config.console = IRB
-      end  
+    if ENV['VIRB'] == 'pry'
+      require 'virb/pry'
+      console do
+        if Rails::VERSION::MAJOR == 3
+          Rails::Console::IRB = Virb::Pry
+        end  
+        if Rails::VERSION::MAJOR == 4
+          Rails.application.config.console = Virb::Pry
+        end  
 
-      if (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 2) || Rails::VERSION::MAJOR == 4
-        require 'rails/console/app'
-        require 'rails/console/helpers'
-        TOPLEVEL_BINDING.eval('self').extend ::Rails::ConsoleMethods
+        if (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 2) || Rails::VERSION::MAJOR == 4
+          require 'rails/console/app'
+          require 'rails/console/helpers'
+          TOPLEVEL_BINDING.eval('self').extend ::Rails::ConsoleMethods
+        end
+      end
+    else
+      console do
+        if Rails::VERSION::MAJOR == 3
+          Rails::Console::IRB = IRB
+        end  
+        if Rails::VERSION::MAJOR == 4
+          Rails.application.config.console = IRB
+        end  
+
+        if (Rails::VERSION::MAJOR == 3 && Rails::VERSION::MINOR >= 2) || Rails::VERSION::MAJOR == 4
+          require 'rails/console/app'
+          require 'rails/console/helpers'
+          TOPLEVEL_BINDING.eval('self').extend ::Rails::ConsoleMethods
+        end
       end
     end
   end
