ed1c58a77a61a042a15958ce3602d82d4664bc17
Daniel Choi <dhchoi@gmail.com>
2012-12-24 11:17:25 -0500

    resize

--- images/scaledown.sh
+++ images/scaledown.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# scales an image file down to 75%
-
+scale=60
 # an ad hoc script that can be adapted for future uses
 
 # $1 name of an jpg image file 
 
 out=${1%.png}-sm.png
 echo "$1 -> $out"
-convert $1 -scale %50 $out
+convert $1 -scale %${scale} $out
