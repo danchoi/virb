8551f7b25d633059c23bfdbcf1c87ed1d59ba1e4
Daniel Choi <dhchoi@gmail.com>
2012-12-24 00:35:18 -0500

    Fake output class wroks with Pry

--- pry.rb
+++ pry.rb
@@ -19,10 +19,38 @@ class Fifo
   end
 end
 
+# fakes a tty stdout
+class Output
+  def initialize
+    @out = File.open("pryout", "w")
+    @out.sync = true
+  end
+  def print s
+    @out.print s
+  end
+  def puts s
+    @out.puts s
+  end
+  def write s
+    puts s
+  end
+  def flush
+  end
+  def tty?
+    true
+  end
+end
+
 Pry.cli = true
 Pry.color = false
+Pry.config.pager = false
 input = Fifo.new
 #input = StringIO.new("puts 1")
 Pry.input = input
+#out = File.open("pryout", "w")
+out = Output.new
+Pry.config.output = out
+$stdout = out
+$stderr = out
 Pry.start(TOPLEVEL_BINDING.eval('self'))
 
