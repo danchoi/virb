4af058f0e590f8bb502e7262d9ff12d51249c87b
Daniel Choi <dhchoi@gmail.com>
2012-12-24 11:17:53 -0500

    Revert "resize"
    This reverts commit ed1c58a77a61a042a15958ce3602d82d4664bc17.

--- images/scaledown.sh
+++ images/scaledown.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-scale=60
+# scales an image file down to 75%
+
 # an ad hoc script that can be adapted for future uses
 
 # $1 name of an jpg image file 
 
 out=${1%.png}-sm.png
 echo "$1 -> $out"
-convert $1 -scale %${scale} $out
+convert $1 -scale %50 $out
