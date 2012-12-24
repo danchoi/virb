03eddb5407617e226eab223b18493822ab016e53
Daniel Choi <dhchoi@gmail.com>
2012-12-24 00:21:17 -0500

    Working on pry

---
+++ pry.rb
@@ -0,0 +1,27 @@
+
+require 'pry'
+#`mkfifo fifo`
+class Fifo
+  def initialize
+    reinit_fifo
+  end
+  def reinit_fifo
+    fd = IO.sysopen("fifo")
+    @io = IO.new(fd, 'r')
+  end
+  def readline(*)
+    if @io.eof?
+      reinit_fifo
+    end
+    @io.gets
+  end
+  def rewind
+  end
+end
+
+Pry.cli = true
+input = Fifo.new
+#input = StringIO.new("puts 1")
+Pry.input = input
+Pry.start(TOPLEVEL_BINDING.eval('self'))
+
